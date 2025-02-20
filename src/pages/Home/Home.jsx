7b54546b3621aaca661e5f3c2243b0b26090a1e4
sajjadhossain <sajjadhossain0756@gmail.com>
2025-02-20 22:48:22 +0600

    basic app set-up done

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from 'react-helmet-async'
+
+const Home = () => {
+  return (
+    <div>
+      <Helmet>
+        <title> PlantNet | Buy Your Desired Plant</title>
+      </Helmet>
+      Home
+    </div>
+  )
+}
+
+export default Home
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,85 @@
+import { createBrowserRouter } from 'react-router-dom'
+import Home from '../pages/Home/Home'
+import ErrorPage from '../pages/ErrorPage'
+import Login from '../pages/Login/Login'
+import SignUp from '../pages/SignUp/SignUp'
+import PrivateRoute from './PrivateRoute'
+import MainLayout from '../layouts/MainLayout'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      }
+    ],
+  },
+  { path: '/login', element: <Login /> },
+  { path: '/signup', element: <SignUp /> },
+  // {
+  //   path: '/dashboard',
+  //   element: (
+  //     <PrivateRoute>
+  //       <DashboardLayout />
+  //     </PrivateRoute>
+  //   ),
+  //   children: [
+  //     {
+  //       index: true,
+  //       element: (
+  //         <PrivateRoute>
+  //           <Statistics />
+  //         </PrivateRoute>
+  //       ),
+  //     },
+  //     {
+  //       path: 'add-plant',
+  //       element: (
+  //         <PrivateRoute>
+  //           <AddPlant />
+  //         </PrivateRoute>
+  //       ),
+  //     },
+  //     {
+  //       path: 'my-inventory',
+  //       element: (
+  //         <PrivateRoute>
+  //           <MyInventory />
+  //         </PrivateRoute>
+  //       ),
+  //     },
+  //     {
+  //       path: 'manage-users',
+  //       element: (
+  //         <PrivateRoute>
+  //           <ManageUsers />
+  //         </PrivateRoute>
+  //       ),
+  //     },
+  //     {
+  //       path: 'profile',
+  //       element: (
+  //         <PrivateRoute>
+  //           <Profile />
+  //         </PrivateRoute>
+  //       ),
+  //     },
+  //     {
+  //       path: 'my-orders',
+  //       element: (
+  //         <PrivateRoute>
+  //           <MyOrders />
+  //         </PrivateRoute>
+  //       ),
+  //     },
+  //     {
+  //       path: 'manage-orders',
+  //       element: <ManageOrders />,
+  //     },
+  //   ],
+  // },
+])
