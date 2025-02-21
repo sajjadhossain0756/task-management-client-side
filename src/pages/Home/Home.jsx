57f932487cdb273c2d8263ca203b7904087ed0af
sajjadhossain <sajjadhossain0756@gmail.com>
2025-02-21 16:20:25 +0600

    delete task from db done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,11 +4,17 @@ import { useForm } from 'react-hook-form'
 import useAxiosPublic from '../../hooks/useAxiosPublic'
 import axios from 'axios'
 import Swal from 'sweetalert2'
+import { useQuery } from '@tanstack/react-query'
+import useAxiosSecure from '../../hooks/useAxiosSecure'
+import { format } from 'date-fns'
+import { MdDelete } from "react-icons/md";
 
 const Home = () => {
   const { register, handleSubmit, control, reset, formState: { errors } } = useForm()
   const axiosPublic = useAxiosPublic()
+  const axiosSecure = useAxiosSecure()
 
+  // create a new task in database
   const onSubmit = async (data) => {
 
     console.log(data)
@@ -35,9 +41,57 @@ const Home = () => {
             showConfirmButton: false,
             timer: 1500
           });
+          refetch()
         }
       })
 
+  }
+  // get all-task from database
+  const { data: tasks = [], refetch } = useQuery({
+    queryKey: ['tasks'],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`http://localhost:3008/all_tasks`)
+      return res.data;
+    }
+  })
+
+  // filter task by category
+  const todoTasks = tasks.filter(task => task.category === 'To Do')
+  const inProgressTasks = tasks.filter(task => task.category === 'In Progress')
+  const doneTasks = tasks.filter(task => task.category === 'Done')
+
+  // delete a task from db
+  const handleDeleteTask = (id) => {
+    console.log(id)
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        try {
+          axios.delete(`http://localhost:3008/all_tasks/${id}`)
+            .then(res => {
+              console.log(res.data)
+              if (res.data.deletedCount > 0) {
+                Swal.fire({
+                  title: "Deleted!",
+                  text: "Your file has been deleted.",
+                  icon: "success"
+                });
+                refetch()
+              }
+            })
+        } catch (err) {
+          Swal.fire('Error', err.message)
+        }
+      }
+
+    });
   }
 
   return (
@@ -49,15 +103,62 @@ const Home = () => {
         <button className='btn' onClick={() => document.getElementById('my_modal_5').showModal()}>Create A Task</button>
       </div>
       <Container>
-        <div className=' flex'>
-          <div className='bg-purple-500 flex-1'>
-            <p className='text-2xl font-bold text-center'>To do</p>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-2 pb-3'>
+          {/* To-do category start here */}
+          <div className='bg-purple-500 p-2'>
+            <p className='text-2xl font-bold text-center mb-3'>To do</p>
+            <div className='space-y-2'>
+              {todoTasks && todoTasks.map(task => <div key={task._id}
+                className='bg-pink-500 p-2 flex gap-3 items-center justify-between rounded'>
+                <div>
+                  <h2 className='font-bold'>{task?.title}</h2>
+                  <p>{task?.description}</p>
+                  <p>{format(new Date(task?.Date), "EEEE, hh:mm a")}</p>
+                </div>
+                <div>
+                  <button onClick={() => { handleDeleteTask(task._id) }}
+                    className=' text-3xl'><MdDelete></MdDelete></button>
+                </div>
+              </div>)}
+            </div>
           </div>
-          <div className='bg-pink-500 flex-1'>
-            <p className='text-2xl font-bold text-center'>In progress</p>
+
+          {/* in progress category start here */}
+          <div className='bg-pink-500 p-2'>
+            <p className='text-2xl font-bold text-center mb-3'>In progress</p>
+            <div className='space-y-2'>
+              {inProgressTasks && inProgressTasks.map(task => <div key={task._id}
+                className='bg-yellow-500 p-2 flex gap-3 items-center justify-between rounded'>
+                <div>
+                  <h2 className='font-bold'>{task?.title}</h2>
+                  <p>{task?.description}</p>
+                  <p>{format(new Date(task?.Date), "EEEE, hh:mm a")}</p>
+                </div>
+                <div>
+                  <button onClick={() => { handleDeleteTask(task._id) }}
+                    className=' text-3xl'><MdDelete></MdDelete></button>
+                </div>
+              </div>)}
+            </div>
           </div>
-          <div className='bg-yellow-500 flex-1'>
-            <p className='text-2xl font-bold text-center'>Done</p>
+
+          {/* done task category start here */}
+          <div className='bg-yellow-500 p-2'>
+            <p className='text-2xl font-bold text-center mb-3'>Done</p>
+            <div className='space-y-2'>
+              {doneTasks && doneTasks.map(task => <div key={task._id}
+                className='bg-purple-500 p-2 flex items-center justify-between rounded'>
+                <div>
+                  <h2 className='font-bold'>{task?.title}</h2>
+                  <p >{task?.description}</p>
+                  <p>{format(new Date(task?.Date), "EEEE, hh:mm a")}</p>
+                </div>
+                <div>
+                  <button onClick={() => { handleDeleteTask(task._id) }}
+                    className=' text-3xl'><MdDelete></MdDelete></button>
+                </div>
+              </div>)}
+            </div>
           </div>
         </div>
         <dialog id="my_modal_5" className="modal modal-middle">
@@ -99,9 +200,9 @@ const Home = () => {
                       className='border bg-white text-gray-700 p-2 rounded-md w-full'
                     >
                       <option value='default'>Select A Category</option>
-                      <option>Java</option>
-                      <option>Go</option>
-                      <option>C</option>
+                      <option>To Do</option>
+                      <option>In Progress</option>
+                      <option>Done</option>
                     </select>
                   </div>
                 </div>
@@ -129,6 +230,12 @@ const Home = () => {
                 </div>
               </form>
             </section>
+            <div className="modal-action">
+              <form method="dialog">
+                {/* if there is a button in form, it will close the modal */}
+                <button className="btn">Close</button>
+              </form>
+            </div>
           </div>
         </dialog>
       </Container>
